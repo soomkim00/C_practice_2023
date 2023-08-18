89ea9d389b5fa2126adb072deed96eb443766a18
Sumin Kim <cm14121453@gmail.com>
2023-08-18 20:06:07 +0900

    23.08.17
    C 독학

---
+++ Myproject/Myproject/loop.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//printf("Hello World\n");
+
+	// ++
+	//int a = 10;
+	//printf("a 는 %d\n", a);
+	//a++;
+	//printf("a 는 %d\n", a);
+
+	// 반복문 : for, while, do while
+
+	// for (선언; 조건; 증감)
+	//for (int i = 1; i <= 10; i++)
+	//{
+	//	printf("Hello World%d\n", i);
+	//}
+	
+	// while (조건) {}
+	//int i = 1;
+	//while (i <= 10)
+	//{
+	//	printf("Hello World%d\n", i);
+	//	i++;
+	//}
+
+	// do while
+	// do {} while (조건);
+	//int i = 1;
+	//do {
+	//	printf("Hello World%d\n", i++);
+	//} while (i <= 10);
+
+	// 2중 반복문 : 구구단
+	//for (int i = 1; i <= 3; i++)
+	//{
+	//	printf("첫 번째 반복문 : %d\n", i);
+	//	for (int j = 1; j <= 5; j++)
+	//	{
+	//		printf("	두 번째 반복문 : %d\n", j);
+	//	}
+	//}
+
+	// 구구단을 외자
+	//for (int i = 2; i <= 9; i++)
+	//{
+	//	printf("%d단\n", i);
+	//	for (int j = 1; j <= 9; j++)
+	//	{
+	//		printf(" %d x %d = %d\n", i, j, i * j);
+	//	}
+	//	printf("\n");
+	//}
+
+	/*
+	* 
+	**
+	***
+	****
+	*****
+	*/
+	//for (int i = 1; i <= 5; i++)
+	//{
+	//	for (int j = 1; j <= i; j++)
+	//	{
+	//		printf("*");
+	//	}
+	//	printf("\n");
+	//}
+
+	/*
+	    *
+	   **
+	  ***
+	 ****
+	*****
+	*/
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i; j < 5 -1; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k <= i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
--- Myproject/Myproject/printscanf.c
+++ Myproject/Myproject/printscanf.c
@@ -42,9 +42,40 @@ int main(void)
 	//printf("%c\n", c);
 
 	// 문자열
-	char str[256];
-	scanf_s("%s", str, sizeof(str));
-	printf("%s", str);
+	//char str[256];
+	//scanf_s("%s", str, sizeof(str));
+	//printf("%s", str);
+
+	// 프로젝트
+	// 경찰관 조서 프로그램 : 입력, 저장, 출력
+	// 이름, 나이, 몸무게, 키, 범죄명
+	char name[256];
+	printf("이름이 뭐에요? ");
+	scanf_s("%s", name, sizeof(name));
+
+	int age;
+	printf("몇살이에요? ");
+	scanf_s("%d", &age);
+
+	float weight;
+	printf("몸무게는 몇 kg 이에요? ");
+	scanf_s("%f", &weight);
+
+	double height;
+	printf("키는 몇 cm 에요? ");
+	scanf_s("%lf", &height);
+
+	char what[256];
+	printf("무슨 범죄를 저질렀어요? ");
+	scanf_s("%s", what, sizeof(what));
+
+	// 조서 내용 출력
+	printf("\n\n--- 범죄자 정보 ---\n\n");
+	printf(" 이름		: %s\n", name);
+	printf(" 나이		: %d\n", age);
+	printf(" 몸무게		: %.2f\n", weight);
+	printf(" 키		: %.2lf\n", height);
+	printf(" 범죄		: %s\n", what);
 
 	return 0;
 }
